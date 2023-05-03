53c624fbac4f2768924df66a996fe278f29d9901
helderleoncio <helderleoncio@gmail.com>
2023-05-03 20:14:26 -0300

    Scripts do dbEscola

--- dbEscola/dbescola.sql
+++ dbEscola/dbescola.sql
@@ -1,3 +1,10 @@
 CREATE DATABASE dbEscola;
 
 USE dbEscola;
+
+CREATE TABLE tbAluno(
+	ALU_CODIGO int not null auto_increment,
+	ALU_NOME varchar(50) not null,
+	ALU_CFP varchar(15) not null,
+	primary key (ALU_CODIGO)
+);
