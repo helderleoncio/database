5eecd78510e502d6db50fc4c9b1a04518af0c2d3
Helder Júnior <helderleoncio@gmail.com>
2025-02-26 19:11:14 -0300

    Database

--- dbLocadora/02_dbLocadora.sql
+++ dbLocadora/02_dbLocadora.sql
@@ -3,15 +3,15 @@ CREATE DATABASE dbLocadora;
 USE dbLocadora;
 
 CREATE TABLE tbCliente(
-	CLI_CODIGO int auto_increment,
-	CLI_CPF char(11),
-	CLI_NOME varchar(20) not null,
-	CLI_DATA_CADASTRO date,
-	CLI_CIDADE varchar(20),
-	CLI_UF char(2) default 'CE',
-	constraint UN_CLI_CPF unique (CLI_CPF),
-	primary key(CLI_CODIGO)
-);
+	CLI_CODIGO int auto_increment comment 'Cógido do cliente',
+	CLI_CPF char(11) comment 'CPF do cliente',
+	CLI_NOME varchar(20) not null comment 'Nome do cliente',
+	CLI_DATA_CADASTRO date comment 'Data de cadastro do cliente',
+	CLI_CIDADE varchar(20) comment 'Cidade do cliente',
+	CLI_UF char(2) default 'CE' comment 'Estado do cliente',
+	constraint UN_CLI_CPF unique (CLI_CPF) comment 'Definindo o CPF como chade unica',
+	primary key(CLI_CODIGO) comment 'Chave primária'
+) comment 'Tabela de cadastro de clientes';
 
 CREATE TABLE tbCategoria(
 	CAT_CODIGO int auto_increment,
@@ -54,7 +54,7 @@ CREATE TABLE tbEmprestimoDevolucao (
 	FIL_CODIGO int,
 	EMD_DATA_EMPRESTIMO date not null,
 	EMD_DATA_DEVOLUCAO_PREVISTA date not null,
-	EMD_DATA_DEVOLUCAO_EFETIVE date,
+	EMD_DATA_DEVOLUCAO_EFETIVA date,
 	EMD_VALOR_MULTA numeric(4,2) default 0,
 	primary key(CLI_CODIGO, FIL_CODIGO, EMD_DATA_EMPRESTIMO),
 	constraint fk_tbEmpDev_tbCliente foreign key (CLI_CODIGO) references tbCliente (CLI_CODIGO)
@@ -62,5 +62,3 @@ CREATE TABLE tbEmprestimoDevolucao (
 	constraint fk_tbEmpDev_tbFilme foreign key (FIL_CODIGO) references tbFilme (FIL_CODIGO)
 	on delete cascade on update cascade
 );
-
-create index IDX_CLI_NOME on tbCliente (CLI_NOME);
