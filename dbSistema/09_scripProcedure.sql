f5b9982b969188b3197901eb67f12618165a6199
Helder Jr <helderleoncio@gmail.com>
2026-09-01 17:46:48 -0300

    Update D-ACO/IDS

--- dbSistema/09_scripProcedure.sql
+++ dbSistema/09_scripProcedure.sql
@@ -1,29 +1,60 @@
+-- Cria uma procedure que consulta e exibe os dados dos clientes
+-- armazenados na view vw_viewClientes:
 DELIMITER $$
 	CREATE PROCEDURE spRelatorioClientes()
 	BEGIN
-		SELECT * FROM tbCliente;
+		SELECT * FROM vw_viewClientes;
 	END $$
 DELIMITER ;
 
+-- Executa a procedure spRelatorioClientes:
 CALL spRelatorioClientes();
 
-
+-- Cria uma procedure que consulta uma quantidade limitada de produtos,
+-- definida pelo parâmetro de entrada QTD:
 DELIMITER $$
-CREATE PROCEDURE exibir_produtos(IN QTD INT)
+CREATE PROCEDURE spConsultaProdutos(IN QTD INT)
 	BEGIN
 		SELECT * FROM tbProduto LIMIT QTD;
 	END $$
 DELIMITER ;
 
-CALL exibir_produtos(10);
+-- Executa a procedure exibindo os 10 primeiros produtos:
+CALL spConsultaProdutos(10);
 
+-- Cria uma procedure que consulta uma quantidade de produtos.
+-- Caso QTD seja NULL, exibe todos os produtos:
 DELIMITER $$
-CREATE PROCEDURE contar_produtos(OUT QTD INT)
+
+CREATE PROCEDURE spConsultaProdutos2(IN QTD INT)
 BEGIN
-	SELECT COUNT(*) INTO QTD FROM tbProduto;
+    IF QTD IS NULL THEN
+        SELECT * FROM tbProduto;
+    ELSE
+        SELECT * FROM tbProduto LIMIT QTD;
+    END IF;
+END $$
+
+DELIMITER ;
+
+-- Exibe os 10 primeiros produtos:
+CALL spConsultaProdutos2(10);
+
+-- Exibe todos os produtos:
+CALL spConsultaProdutos2(NULL);
+
+-- Cria uma procedure que conta a quantidade total de produtos
+-- e armazena o resultado no parâmetro de saída QTD:
+DELIMITER $$
+CREATE PROCEDURE spContarProdutos(OUT QTD INT)
+BEGIN
+    SELECT COUNT(*) INTO QTD 
+    FROM tbProduto;
 END $$
 DELIMITER ;
 
-CALL contar_produtos(@QTD);
+-- Executa a procedure e armazena o resultado na variável @QTD:
+CALL spContarProdutos(@QTD);
+-- Exibe a quantidade de produtos armazenada na variável @QTD:
 SELECT @QTD;
 
